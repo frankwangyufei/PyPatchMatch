f38b2baef5ef54c73fece002dc8debe56ab946b9
Frank Wang <frankwangyufei559@gmail.com>
2021-01-03 22:15:58 +0800

    updates Makefile to opencv4

--- examples/cpp_example_run.sh
+++ examples/cpp_example_run.sh
@@ -8,8 +8,8 @@
 
 set -x
 
-CFLAGS="-std=c++14 -O2 $(pkg-config --cflags opencv)"
-LDFLAGS="$(pkg-config --libs opencv)"
+CFLAGS="-std=c++14 -O2 $(pkg-config --cflags opencv4)"
+LDFLAGS="$(pkg-config --libs opencv4)"
 g++ $CFLAGS cpp_example.cpp -I../csrc/ -L../ -lpatchmatch $LDFLAGS -o cpp_example.exe
 LD_LIBRARY_PATH=../:$LD_LIBRARY_PATH time ./cpp_example.exe
 
